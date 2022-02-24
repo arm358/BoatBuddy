3feabbe2d9f7d6dbf6d37536b31408f8012ff84e
Andrew <andrmcl9@gmail.com>
2022-02-24 09:15:01 -0500

    rename tiles files

--- boatbuddy/settings.py
+++ boatbuddy/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "x"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
